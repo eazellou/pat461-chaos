e576830103eb33a43d4892cf1988fd098bc71de4
eazellou <emmaazelborn@gmail.com>
2015-03-17 19:24:42 -0400

    display blue dots, start on Harmony Page

--- harmonyChaos.lua
+++ harmonyChaos.lua
@@ -1,6 +1,7 @@
 --basic structure declares two pages with different color backgrounds.
 
 FreeAllRegions()
+DPrint("")
 
 r = nil
 local currentpage = 0
@@ -23,7 +24,7 @@ SetPage(1)
 currentpage = 1
 --harmony
 r1 = Region()
-r1.t = r1:Texture(0,128,255,255) --sky blue
+r1.t = r1:Texture(32,32,32,255)
 r1:EnableHorizontalScroll(true)
 r1:Handle("OnHorizontalScroll", SwitchPage)
 r1:Show()
@@ -32,6 +33,57 @@ r1:SetWidth(ScreenWidth())
 r1:SetHeight(ScreenHeight())
 r1:SetAnchor("BOTTOMLEFT",0,0)
 r1:SetLayer("BACKGROUND")
+
+smallHeight = ScreenHeight()/3 - 8
+if smallHeight > ScreenWidth()/2 then
+	smallHeight = ScreenWidth()/2
+end
+
+bigHeight = ScreenHeight()/2 - 6
+if bigHeight > ScreenWidth()/2 then
+	bigHeight = ScreenWidth()/2
+end
+bigRadius = bigHeight/2
+
+dot1 = Region()
+dot1.t = dot1:Texture(DocumentPath("Dot.png"))
+dot1:Show() 
+dot1.t:SetBlendMode("ALPHAKEY")
+dot1:SetHeight(smallHeight)
+dot1:SetWidth(dot1:Height())
+
+dot2 = Region()
+dot2.t = dot2:Texture(DocumentPath("Dot.png"))
+dot2:Show() 
+dot2.t:SetBlendMode("ALPHAKEY")
+dot2:SetHeight(smallHeight)
+dot2:SetWidth(dot2:Height())
+dot2:SetAnchor("BOTTOMLEFT", 0, dot1:Height() + 12)
+
+dot3 = Region()
+dot3.t = dot3:Texture(DocumentPath("Dot.png"))
+dot3:Show() 
+dot3.t:SetBlendMode("ALPHAKEY")
+dot3:SetHeight(smallHeight)
+dot3:SetWidth(dot3:Height())
+dot3:SetAnchor("BOTTOMLEFT", 0, dot1:Height() + dot2:Height() + 24)
+
+dot5 = Region()
+dot5.t = dot5:Texture(DocumentPath("Dot.png"))
+dot5:Show() 
+dot5.t:SetBlendMode("ALPHAKEY")
+dot5:SetHeight(bigHeight)
+dot5:SetWidth(dot5:Height())
+dot5:SetAnchor("CENTER", ScreenWidth() - bigRadius, ScreenHeight()/2 - bigRadius - 6)
+
+dot6 = Region()
+dot6.t = dot6:Texture(DocumentPath("Dot.png"))
+dot6:Show() 
+dot6.t:SetBlendMode("ALPHAKEY")
+dot6:SetHeight(bigHeight)
+dot6:SetWidth(dot6:Height())
+dot6:SetAnchor("CENTER", ScreenWidth() - bigRadius, ScreenHeight()/2 + bigRadius + 6)
+
 
 halfWidth = ScreenWidth() / 2
 halfHeight = ScreenHeight() / 2
@@ -62,7 +114,7 @@ function chaosMovement(region, x, y, z)
 
 	if changeInStrength > maxStrength then
 		maxStrength = changeInStrength
-		DPrint("Max strength: " .. tostring(maxStrength))
+		--DPrint("Max strength: " .. tostring(maxStrength))
 	end
 
 	if changeInStrength > MAXSTRENGTHPOSSIBLE then
@@ -94,3 +146,5 @@ middleCircle:EnableInput(true)
 middleCircle:SetWidth(50)
 middleCircle:SetHeight(50)
 middleCircle:SetAnchor("TOP", halfWidth, halfHeight)
+
+SetPage(1)
